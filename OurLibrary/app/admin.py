72c55de43438dabf824ec3681a675164bc3c34e8
theanh <=>
2022-07-29 11:21:24 +0700

    add

---
+++ OurLibrary/app/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *
+from . import forms, models
+
+# Register your models here.
+
+class MyUserAdmin(UserAdmin):
+    add_form = forms.MyUserCreationForm
+    form = forms.MyUserChangeForm
+    model = models.User
+    list_display = ['username', 'email', 'fullname', 'phone']
+
+admin.site.register(models.User, MyUserAdmin)
+admin.site.register(Author)
+admin.site.register(Publisher)
+admin.site.register(Category)
+admin.site.register(Book)
+admin.site.register(BookRent)
+
---
+++ OurLibrary/app/views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from .models import User
+from .forms import MyUserCreationForm,MyUserChangeForm
+
+# Create your views here.
+def signup(request):
+    form = MyUserCreationForm()
+   
+    if request.method == 'POST':
+       form = MyUserCreationForm(request.POST)
+       if form.is_valid():
+            user = form.save()
+            user = authenticate(username=user.username,
+                                    password=request.POST['password1'])
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'registration/signup.html', { 'form':  form})
+
+
+def profile(request):
+    if request.method =='POST':
+        form = MyUserChangeForm(request.POST,instance= request.user)
+        if form.is_valid():
+            form.save()
+    form = MyUserChangeForm(instance= request.user)
+    return render(request, 'registration/profile.html',{'form':form})
